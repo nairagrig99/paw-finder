181332c7ab749a185f2aa4aae193efc9d372de8d
nairagrig99 <nairagrig1999@gmail.com>
2026-09-03 14:09:27 +0400

    refactor(hooks): extract logic into custom hook and introduce global constants

---
+++ src/hooks/useConvertDate.ts
@@ -0,0 +1,55 @@
+import {useEffect, useState} from "react";
+import {DAY, MILLISECOND, MINUTE, MONTH, YEAR} from "../constant.ts";
+
+export default function useConvertDate() {
+
+    const [dateNow, setDateNow] = useState<number>(0);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDateNow(Date.now());
+        }, 1000);
+
+        return () => clearTimeout(timer)
+    }, []);
+
+    //idempotency problem
+    return (createdAt: string) => {
+
+        const createdTime = new Date(createdAt).getTime();
+        const differencesInSecond = Math.floor((dateNow - createdTime) / MILLISECOND);
+
+        const hour = MINUTE * MINUTE;
+        const day = hour * DAY;
+        const month = day * MONTH;
+        const year = day * YEAR;
+
+        if (differencesInSecond < MINUTE) {
+            return `${differencesInSecond} seconds ago`;
+        }
+
+        if (differencesInSecond < hour) {
+            const diffMinutes = Math.floor(differencesInSecond / MINUTE)
+            return `${diffMinutes} ${diffMinutes === 1 ? "minute" : "minutes"} ago`;
+        }
+
+        if (differencesInSecond < day) {
+            const diffHours = Math.floor(differencesInSecond / hour);
+            return `${diffHours} ${diffHours === 1 ? "hour" : "hours"} ago`;
+        }
+
+        if (differencesInSecond < month) {
+            const diffDays = Math.floor(differencesInSecond / day);
+            return `${diffDays} ${diffDays === 1 ? "day" : "days"} ago`;
+        }
+
+        if (differencesInSecond < year) {
+            const diffMonths = Math.floor(differencesInSecond / month);
+            return `${diffMonths} ${diffMonths === 1 ? "month" : "months"} ago`;
+        }
+
+        const years = Math.floor(differencesInSecond / year);
+        return `${years} ${years === 1 ? "year" : "years"} ago`;
+    }
+
+}
