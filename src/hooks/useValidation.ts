c5fe430accf5e98786aea7904e8711cc322cf56f
nairagrig99 <nairagrig1999@gmail.com>
2026-09-10 17:05:16 +0400

    feat:integrate useActionState and form field validation

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -1,23 +1,32 @@
 import {useState} from "react";
-import type {FormElementType, Report} from "../types/report.ts";
+import type {FormElementType} from "../types/report.ts";
 
-export default function useValidation(form: Report) {
+export default function useValidation() {
 
-    const [error, setError] = useState(form);
-    const validateForm = (event: React.ChangeEvent<FormElementType>) => {
-        const {name, value} = event.target
+    const [error, setError] = useState<Record<string, string | number>>({});
+    const validation = (name: string, value: string) => {
         if (value === '') {
             setError((prevState) => ({
                 ...prevState,
                 [name]: 'This field is required'
             }))
         } else {
-            setError((prevState) => ({
-                ...prevState,
-                [name]: ''
-            }))
+            setError((prevState) => {
+                const nextState = {...prevState}
+                delete nextState[name]
+                return nextState
+            })
         }
     }
+    const validateForm = (event: React.ChangeEvent<FormElementType>) => {
+        const {name, value} = event.target
+        validation(name, value)
+    }
+    const validateFormByName = (name: string, value: string) => {
 
-    return {error, validateForm}
+        validation(name, value)
+    }
+
+    console.log('error', error);
+    return {error, validateForm,validateFormByName}
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-    tailwindcss()
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/db.json'],
+        },
+    },
 })
